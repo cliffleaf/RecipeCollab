53d367fa0b08b0bc905296d8531a92c50e91427a
Kevin Liang <kevinliang0430@gmail.com>
2024-04-23 20:09:10 +1000

    initialised backend directory

---
+++ recipe-backend/src/main/java/personal/cliffleaf/recipecollab/RecipeCollabApplication.java
@@ -0,0 +1,13 @@
+package personal.cliffleaf.recipecollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeCollabApplication.class, args);
+	}
+
+}
---
+++ recipe-backend/src/test/java/personal/cliffleaf/recipecollab/RecipeCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.cliffleaf.recipecollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
