ee8489a250ab6da803d424cd72b16b30c0db0ba4
Kevin Liang <53232169+cliffleaf@users.noreply.github.com>
2024-04-23 21:34:32 +1000

    Established backend structure (#5)
    * established backend structure
    
    * completed repository and DDB config
    
    * finished service & controller class, haven't tested yet

---
+++ recipe-backend/src/main/java/personal/cliffleaf/recipecollab/recipe/RecipeController.java
@@ -0,0 +1,38 @@
+package personal.cliffleaf.recipecollab.recipe;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/recipe")
+public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @PostMapping("/upload")
+    public Recipe uploadRecipe(@RequestBody Recipe recipe) {
+        return recipeService.saveRecipe(recipe);
+    }
+
+    @GetMapping("/{id}")
+    public Recipe getRecipe(@PathVariable String id) {
+        return recipeService.getRecipeById(id);
+    }
+
+    @PutMapping("/edit/{id}")
+    public Recipe editRecipe(@PathVariable String id, @RequestBody Recipe newRecipe) {
+        return recipeService.updateRecipe(id, newRecipe);
+    }
+
+    @GetMapping
+    public List<Recipe> getRecipesByCategory(@RequestParam String category) {
+        return recipeService.getRecipesByCategory(category);
+    }
+
+    @GetMapping("/all")
+    public List<Recipe> getAllRecipes() {
+        return recipeService.getAllRecipes();
+    }
+}
