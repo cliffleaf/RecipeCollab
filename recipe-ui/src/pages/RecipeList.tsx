adeb7dd5f228f16cf636cd3eb162da02a663e06d
Kevin Liang <kevinliang0430@gmail.com>
2024-09-25 22:57:40 +1000

    Added search bar to recipe list

--- recipe-ui/src/pages/RecipeList.tsx
+++ recipe-ui/src/pages/RecipeList.tsx
@@ -5,6 +5,7 @@ import { API_BASE_URL } from '../config';
 import {useEffect, useState} from "react";
 import axios, {AxiosError, AxiosResponse} from 'axios';
 import "../css/RecipeList.css";
+import EditorSearchIcon from '@atlaskit/icon/glyph/editor/search'
 
 type Recipe = {
     id: string;
@@ -25,7 +26,7 @@ const RecipeList = () => {
     const recipes = [
         {
             id: "",
-            title: "Spaghetti Bolognese",
+            title: "西红柿炒鸡蛋",
             author: "东街一只猫",
             imageUrl: "https://img.taste.com.au/_e6onvZ7/w720-h480-cfill-q80/taste/2024/03/5-ingredient-turbo-charged-spaghetti-recipe-196959-1.jpg",
         },
@@ -64,15 +65,28 @@ const RecipeList = () => {
                     <div className="app-side-nav">
                         <SideNav/>
                     </div>
-                    <div className="app-recipe-card-container">
-                        {recipes.map((recipe: Recipe) => (
-                            <CommunityRecipeCard
-                                id={recipe.id}
-                                title={recipe.title}
-                                author={recipe.author}
-                                imgUrl={recipe.imageUrl}
+                    <div className="content">
+                        <div className="search-bar-wrapper">
+                            <div className="search-icon-wrapper">
+                                <EditorSearchIcon label="" />
+                            </div>
+                            <input
+                                type="text"
+                                className="search-input"
+                                placeholder="Search recipes..."
                             />
-                        ))}
+                        </div>
+                        
+                        <div className="app-recipe-card-container">
+                            {recipes.map((recipe: Recipe) => (
+                                <CommunityRecipeCard
+                                    id={recipe.id}
+                                    title={recipe.title}
+                                    author={recipe.author}
+                                    imgUrl={recipe.imageUrl}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </section>
             </div>
